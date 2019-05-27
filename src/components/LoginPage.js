60bd24f00997d53b6c615b1c5c4f3c10c85a01e9
Jhon Jairo <jjmuesesq@unal.edu.co>
2019-05-27 01:53:18 -0500

    actualizando vista LoginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,20 +5,20 @@ function LoginPage() {
   return (
     <div className="LoginPage">
       <header className="LoginPage-header">   
-        <h1>Login Here</h1>  
+        <h1>Iniciar Sesión</h1>  
         <form>
             {/**USERNAME */}
-            <label for= "username">Username</label>
-            <input type="text" placeholder="Enter Username"/>
+            <label for= "username">Usuario</label>
+            <input type="text" placeholder="Ingresar usuario"/>
 
             {/**PASSWORD  */}
-            <label for= "password">Password</label>
-            <input type="password" placeholder="Enter Password"/>
+            <label for= "password">Contraseña</label>
+            <input type="password" placeholder="Ingresar contraseña"/>
 
-            <input type= "submit" value="Log In"/> 
+            <input type= "submit" value="Acceder"/> 
 
-            <a href= "#">Lost your password</a>
-            <a href= "#">Don't have an account?</a>
+            {/** <a href= "#">Lost your password</a>
+            <a href= "#">Don't have an account?</a>*/}
 
         </form>   
       </header>    
