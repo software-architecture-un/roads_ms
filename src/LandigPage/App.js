d9f56e90b8d427a70e92764a58283ae23a6a660a
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2019-05-26 23:44:59 -0500

    Create ActivitiesPage
    Create ActivitiesPage and update style

--- src/LandigPage/App.js
+++ src/LandigPage/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 // import LandingPage from '../LandigPage/MainPage/LandingPage'
-import GalleryPage from '../LandigPage/GalleryPage/GalleryPage';
+// import GalleryPage from '../LandigPage/GalleryPage/GalleryPage';
+import ActivitiesPage from '../LandigPage/ActivitiesPage/ActivitiesPage';
 
 function App() {
   return (
     <div className="App">
      
-      <GalleryPage/>
+      <ActivitiesPage/>
 
     </div>
   );
