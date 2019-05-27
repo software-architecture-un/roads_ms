c4dab6b4a8c5e0d21d29b2c4e1bd3434e051d599
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2019-05-26 22:18:19 -0500

    Create GalleryPage
    create galleryPage and update styles

--- src/LandigPage/App.js
+++ src/LandigPage/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import LandingPage from '../LandigPage/MainPage/LandingPage'
+// import LandingPage from '../LandigPage/MainPage/LandingPage'
+import GalleryPage from '../LandigPage/GalleryPage/GalleryPage';
 
 function App() {
   return (
     <div className="App">
      
-      <LandingPage/>
+      <GalleryPage/>
 
     </div>
   );
