1d5c4e5944048676281abf8824045e840781537d
jjmuesesq <jjmuesesq@unal.edu.co>
2019-06-03 16:23:14 -0500

    configurando conexion graphQL

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,7 @@ function App() {
   return (
     <div className="App">   
      
-      <LandingPage/>
-        
-      {/* <Register/> */}
-      {/* <LoginPage/> */}
+      <LandingPage/>          
         
     </div>
   );
