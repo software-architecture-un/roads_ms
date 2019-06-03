6a4f1bd1def465d9f32f1ba82a5eb983a291343c
Gonzalo Eduardo Baez Sabogal <gebaezs@unal.edu.co>
2019-06-03 17:23:11 -0500

    udpate

--- src/LandigPage/MainPage/LandingPage.js
+++ src/LandigPage/MainPage/LandingPage.js
@@ -4,14 +4,14 @@ import Camara from '../../Imagenes/camara1.png';
 import Ruta from '../../Imagenes/rutas1.png';
 import Actividad from '../../Imagenes/actividad1.png';
 
-function LandingPage(){
-    return(
+function LandingPage() {
+    return (
         <div className="LandingPage">
-            
+
             <div className="HeaderLandig">
                 <div className="PosicionBotones">
                     <a className="BotonIniciarSesion" href="/#"> Iniciar sesion </a>
-                    
+
                     <a className="BotonCrearCuenta" href="/#"> Crear cuenta </a>
                 </div>
                 <h1 className="TituloLanding"> Senderos UN </h1>
@@ -20,7 +20,7 @@ function LandingPage(){
             <div className="BodyLandig">
                 <div className="CajaGaleria">
                     <h2 className="SubTitulos">Galeria De Fotos</h2>
-                    <img  src={ Camara } width="250" height="250" alt="Imagen_Camara"></img>
+                    <img src={Camara} width="250" height="250" alt="Imagen_Camara"></img>
                     <div className="TextoJustificado">
                         <p>En nuestra galeria podras las fotos que mas les han gustado a nuestros usuarios.</p>
                         <p>Muestranos como ves el mundo.</p>
@@ -29,7 +29,7 @@ function LandingPage(){
 
                 <div className="CajaImagenes">
                     <h2 className="SubTitulos">Rutas</h2>
-                    <img src={ Ruta } width="250" height="250" alt="Imagen_Ruta" ></img>
+                    <img src={Ruta} width="250" height="250" alt="Imagen_Ruta" ></img>
                     <div className="TextoJustificado">
                         <p>Aqui encontraras las rutas preferidas por los usarios.</p>
                         <p>Camina, conduce y descubre los caminos que el mundo tiene preparado para ti-</p>
@@ -39,7 +39,7 @@ function LandingPage(){
 
                 <div className="CajaActividades">
                     <h2 className="SubTitulos">Actividades</h2>
-                    <img className="ImagenActividades" src={ Actividad } width="200" height="200" alt="Imagen_Ruta" ></img>
+                    <img className="ImagenActividades" src={Actividad} width="200" height="200" alt="Imagen_Ruta" ></img>
                     <div className="TextoJustificado">
                         <p>Comparte en familia y con amigos de miles de actividades que estan esperando por ti.</p>
                         <p>Cambia tu rutina ven y descubre lo que tenemos preparado para ti.</p>
@@ -48,8 +48,8 @@ function LandingPage(){
             </div>
 
             <div className="FooterLandig">
-                    <h3 className="Universidad">Universidad Nacional De Colombia</h3>
-                    <h4 className="Materia">Arquitecura De software</h4> 
+                <h3 className="Universidad">Universidad Nacional De Colombia</h3>
+                <h4 className="Materia">Arquitecura De software</h4>
             </div>
 
         </div>
--- src/UserPage/UserPage.js
+++ src/UserPage/UserPage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './UserPage.css';
 
 function UserPage() {
+
     return (
         <div className="UserPage">
 
             <div className="BarraMenuLateral">
 
                 <div className="MiniDatoUsuario">
-                    <img className="FotoPerfil" width="160" height="160"></img>
+                    <img className="FotoPerfil" alt="" width="160" height="160"></img>
                     <h2 className="NombreUsuario">Fulanito Perez</h2>
                 </div>
 
@@ -38,11 +39,35 @@ function UserPage() {
 
             </div>
 
-            <div className="ObjetivoMenuLateral">
+            <div className="ContenedorOpcionDatosPersonales">
+                <div className="TituloDatosPersonales">
+                    <h1>Datos Personales</h1>
+                </div>
+
+                <div className="ContenedorDatosPersonales">
+                    {/* <div className="InformacionDatosPersonales">
+
+                    </div> */}
+
+                    <div className="ContenedorDato">
+                        <label className="LabelNombre">Nombre:</label>
+                        <input className="InputNombre" />
+                    </div>
+
+                    <div className="ContenedorDato">
+                        <label className="LabelNombre">Nombre:</label>
+                        <input className="InputNombre" />
+                    </div>
+
+
+                    <div className="BotonesModificarDatos">
+
+                    </div>
+                </div>
 
             </div>
 
-        </div>
+        </div >
     );
 }
 
